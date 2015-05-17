53b2b036ee3ae2e38675a2fa89c174e25cbf7bca
paulwoodman <paulwoodman@gmail.com>
2015-05-17 03:40:03 +0000

    Uploaded the variable example

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python basics
+
+Nothing interesting. Just writing basic scripts to better understand and learn Python
+
+
---
+++ variable_ex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Basic script to understand that a variable is something that can change.
+# In this example I have used if/else statment to help understand
+
+#import the random module
+from random import randint
+
+# x is the variable. Each time the script is run it will generate
+# number beween 1 to 10. 
+x = randint(1,10)
+
+# the if state will print if x is less than 6
+if x < 6:
+  print 'Under 6'  
+# the elif will print if x is equal to 6
+elif x == 6:
+  print 'It was a six! grab a cookie'
+# The elif will print if x is greater than 6
+elif x > 6:
+  print "Over 6"
